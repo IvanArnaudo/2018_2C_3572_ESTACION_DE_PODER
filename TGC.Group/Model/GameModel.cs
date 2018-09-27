89134ccf3b3f20872f6ccc2e3741caa26a1ef043
RBio <bio.ronan@gmail.com>
2018-09-27 00:52:35 -0300

    copy

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -14,6 +14,7 @@ using TGC.Core.Collision;
 using System.Windows.Forms;
 using TGC.Examples.Example;
 using System.Reflection;
+using TGC.Examples.Collision.SphereCollision;
 
 namespace TGC.Group.Model
 {
@@ -45,10 +46,13 @@ namespace TGC.Group.Model
         private readonly List<TgcMesh> objectsBehind = new List<TgcMesh>();
         private readonly List<TgcMesh> objectsInFront = new List<TgcMesh>();
         float jump = 0;
+        bool techo = false;
+        private SphereCollisionManager collisionManager;
+        private TgcMesh collider;
 
         //private TgcMesh plataforma1, plataforma2;
 
-       // private TgcBoundingSphere characterSphere;
+        // private TgcBoundingSphere characterSphere;
 
         public GameModel(string mediaDir, string shadersDir) : base(mediaDir, shadersDir)
         {
@@ -152,7 +156,6 @@ namespace TGC.Group.Model
 
             var moveForward = 0f;
             float rotate = 0;
-            jump = 0;
             moving = false;
 
             moveForward = MovimientoAbajo() - MovimientoArriba();
@@ -169,6 +172,8 @@ namespace TGC.Group.Model
                 jump = jumping;
                 moving = true;
             }
+            else
+                jump = 0;
 
             //Si hubo rotacion
             if (rotating)
@@ -189,12 +194,12 @@ namespace TGC.Group.Model
                 //Aplicar movimiento hacia adelante o atras segun la orientacion actual del Mesh
                 var lastPos = personajePrincipal.Position;
                 var pminPersonaje = personajePrincipal.BoundingBox.PMin.Y;
-
+                var pmaxPersonaje = personajePrincipal.BoundingBox.PMax.Y;
 
 
                 Movimiento = new TGCVector3(FastMath.Sin(personajePrincipal.Rotation.Y) * moveForward, jump, FastMath.Cos(personajePrincipal.Rotation.Y) * moveForward);
                 personajePrincipal.Move(Movimiento);
-                DetectarColisiones(lastPos, pminPersonaje);
+                DetectarColisiones(lastPos, pminPersonaje, pmaxPersonaje);
 
             }else
             {
@@ -255,7 +260,7 @@ namespace TGC.Group.Model
 
         }
 
-        private void DetectarColisiones(TGCVector3 lastPos, float pminYAnteriorPersonaje)
+        private void DetectarColisiones(TGCVector3 lastPos, float pminYAnteriorPersonaje, float pmaxYAnteriorPersonaje)
         {
             var collisionFound = false;
 
@@ -278,18 +283,62 @@ namespace TGC.Group.Model
                     {
                         enElPiso = true;
                         lastPos.Y = sceneMeshBoundingBox.PMax.Y + 1;
+                        techo = false;
                     }
-
+                    else if (sceneMeshBoundingBox.PMin.Y > pmaxYAnteriorPersonaje)
+                        techo = true;
                     collisionFound = true;
-                    break;
+                    collider = mesh;
+                    var movementRay = lastPos - personajePrincipal.Position;
+                    //Luego debemos clasificar sobre que plano estamos chocando y la direccion de movimiento
+                    //Para todos los casos podemos deducir que la normal del plano cancela el movimiento en dicho plano.
+                    //Esto quiere decir que podemos cancelar el movimiento en el plano y movernos en el otros.
+
+                    var rs = TGCVector3.Empty;
+                    if (((personajePrincipal.BoundingBox.PMax.X > collider.BoundingBox.PMax.X && movementRay.X > 0) ||
+                        (personajePrincipal.BoundingBox.PMin.X < collider.BoundingBox.PMin.X && movementRay.X < 0)) &&
+                        ((personajePrincipal.BoundingBox.PMax.Z > collider.BoundingBox.PMax.Z && movementRay.Z > 0) ||
+                        (personajePrincipal.BoundingBox.PMin.Z < collider.BoundingBox.PMin.Z && movementRay.Z < 0)))
+                    {
+
+                        if (personajePrincipal.Position.X > collider.BoundingBox.PMin.X && personajePrincipal.Position.X < collider.BoundingBox.PMax.X)
+                        {
+                            rs = new TGCVector3(movementRay.X, movementRay.Y, 0);
+                        }
+                        if (personajePrincipal.Position.Z > collider.BoundingBox.PMin.Z && personajePrincipal.Position.Z < collider.BoundingBox.PMax.Z)
+                        {
+                            rs = new TGCVector3(0, movementRay.Y, movementRay.Z);
+                        }
+
+                        //Seria ideal sacar el punto mas proximo al bounding que colisiona y chequear con eso, en ves que con la posicion.
+                    }
+                    else
+                    {
+                        if ((personajePrincipal.BoundingBox.PMax.X > collider.BoundingBox.PMax.X && movementRay.X > 0) ||
+                            (personajePrincipal.BoundingBox.PMin.X < collider.BoundingBox.PMin.X && movementRay.X < 0))
+                        {
+                            rs = new TGCVector3(0, movementRay.Y, movementRay.Z);
+                        }
+                        if ((personajePrincipal.BoundingBox.PMax.Z > collider.BoundingBox.PMax.Z && movementRay.Z > 0) ||
+                            (personajePrincipal.BoundingBox.PMin.Z < collider.BoundingBox.PMin.Z && movementRay.Z < 0))
+                        {
+                            rs = new TGCVector3(movementRay.X, movementRay.Y, 0);
+                        }
+                    }
+                    personajePrincipal.Position = lastPos - rs;
+                    //break;
                 }
             }
             if (collisionFound)
             {
-                if (!enElPiso)
+                /*if (!enElPiso)
+                {
+                    if (techo)
+                        jump = -2 * ElapsedTime;
                     lastPos.Y += jump;
-                personajePrincipal.Position = lastPos;
-
+                }
+                personajePrincipal.Position = lastPos;*/
+                
             }
             else
                 enElPiso = false;
